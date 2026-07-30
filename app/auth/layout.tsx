884f2e22c7514d2dca856e64856f1796ca5748a4
Asif Forizy <asifforizy@gmail.com>
2026-07-31 00:49:27 +0600

    login functionality added

---
+++ app/auth/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AuthLayout = (
+    {
+        children
+    }: {
+        children: React.ReactNode
+    }
+) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default AuthLayout
